48831db2ab0ac629ec2412aa32216aa35d146a85
john.kim <glorifiedjx@gmail.com>
2019-05-30 14:38:35 +0900

    4번문제. cicd.txt

--- run.sh
+++ run.sh
@@ -4,3 +4,6 @@ docker network create --subnet=192.168.43.0/8 mynet
 docker run --net mynet --ip 192.168.43.11 --hostname surl --name shorten-url -d -p 5000:5000 flask/shortenurl
 
 #docker run -d --net mynet123--ip 172.18.0.11 --hostname rmq1 --name rmq_container_name -p 15673:15672 rabbitmq:3-management
+#Try to do
+#Local port forwarding
+# 
